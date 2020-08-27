f30aad309542726442eadc894d43e253a07ba539
NelliPestych <NelliPestych@bigmir.net>
2020-08-27 13:57:29 +0300

    phonebook5

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Phonebook from "./phonebookFolder/Phonebook";
+
+ReactDOM.render(
+    <Phonebook />,
+  document.getElementById('root')
+);
